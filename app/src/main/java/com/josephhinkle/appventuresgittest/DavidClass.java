8bdbb2202a001cafcbd255db5e34f41fc792c23f
Jacob Krevat <Jacob Krevat>
2016-02-20 13:45:37 -0500

    Added a button to DavidActivity

--- app/src/main/java/com/josephhinkle/appventuresgittest/DavidClass.java
+++ app/src/main/java/com/josephhinkle/appventuresgittest/DavidClass.java
@@ -1,8 +1,10 @@
 package com.josephhinkle.appventuresgittest;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 /**
  * Created by davidjohn on 2/19/16.
@@ -16,6 +18,11 @@ public class DavidClass extends Activity{
         setContentView(R.layout.david_activity);
 
 
+    }
+
+    public void pressMeClick(View view) {
+        Intent myIntent = new Intent(DavidClass.this, MainActivity.class);
+        startActivity(myIntent);
     }
 
 
