1ef4f3da3cb78cf22485b17a18d98647ec6cd552
TSNichols <talcott.nichols@gmail.com>
2016-03-04 16:31:52 -0500

    Adding some stuff to test

--- app/src/main/java/com/josephhinkle/appventuresgittest/MainActivity.java
+++ app/src/main/java/com/josephhinkle/appventuresgittest/MainActivity.java
@@ -10,11 +10,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
     private int green;
     private int blue;
+
+    Button mbtn_Add;
+    EditText meTxt_ArrayWords;
+    Spinner mspin_Array;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
